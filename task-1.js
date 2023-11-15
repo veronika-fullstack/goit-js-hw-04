039252086a09ff44d9538ac6b2c344c29a8e5fd3
veronika-fullstack <veronika.bohdanova.it@gmail.com>
2023-11-15 05:24:20 +0200

    complete-hw-4

--- task-1.js
+++ task-1.js
@@ -1,15 +1,24 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let priceSum = quantity * pricePerDroid;
-    if (customerCredits >= priceSum) {
-        return (`You ordered ${quantity} droids worth ${priceSum} credits!`);
-    }
-    else {
-        return (`Insufficient funds!`);
-    }
+function isEnoughCapacity(products, containerSize) {
+  let numberOfProducts = 0;
+  const values = Object.values(products);
+  for (const key of values) {
+    numberOfProducts += key;
+  };
+  return (numberOfProducts <= containerSize);
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
